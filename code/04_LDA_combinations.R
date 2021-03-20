7ac375fa16313c12257bfa655d8c18fe4154add7
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-03-21 00:30:32 +0100

    refactor 'KS_test' to separate file

--- code/04_LDA_combinations.R
+++ code/04_LDA_combinations.R
@@ -5,27 +5,8 @@ source("code/utils.R")
 # read and process data
 data_csv = read.csv2("data/Spectra.csv")
 data = transform_data(data_csv)
-
-# initialize empty vectors
-D_vec = numeric()
-p.value_vec = numeric()
-
-# Kolmogorov–Smirnov test
-for (i in seq.int(2, ncol(data))) {
-
-  ks_test = ks.test(data[data$Species == "Diachrysia chrysitis", i],
-                    data[data$Species == "Diachrysia stenochrysis", i])
-
-  D_vec = c(D_vec, ks_test$statistic)
-  p.value_vec = c(p.value_vec, ks_test$p.value)
-
-}
-
-ks_df = data.frame(band = colnames(data)[-1], D = D_vec, p.value = p.value_vec)
-ks_df = ks_df[order(ks_df$D, decreasing = TRUE), ]
-ks_df$scale = ifelse(substr(ks_df$band, 1, 1) == "G", "Glass", "Brown")
+ks_df = read.csv2("results/ks-test.csv")
 ks_df$scale = as.factor(ks_df$scale)
-ks_df[1:20, ]
 
 # number of bands
 nbands = 8
@@ -34,10 +15,11 @@ nbands = 8
 glass_vec = ks_df[ks_df$scale == "Glass", "band"][seq_len(nbands)]
 brown_vec = ks_df[ks_df$scale == "Brown", "band"][seq_len(nbands)]
 
-# bands combinations
+# initialize empty lists
 glass_comb = list()
 brown_comb = list()
 
+# bands combinations
 for (i in seq.int(2, nbands)) {
 
   comb = combn(glass_vec, i, simplify = FALSE)
