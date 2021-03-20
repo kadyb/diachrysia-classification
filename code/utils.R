bee5b5dc360f2e6138c634ffa81957d44adf5499
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-03-20 22:34:04 +0100

    capital letters in scales

--- code/utils.R
+++ code/utils.R
@@ -12,9 +12,9 @@ transform_data = function(data) {
   data_SG = data.frame(Species = data[, 2], data_SG)
 
   # create wide data.frame
-  brown = data_SG[data$Scale == "brown", ]
+  brown = data_SG[data$Scale == "Brown", ]
   colnames(brown)[2:1698] = paste0("B", 402:2098)
-  glass = data_SG[data$Scale == "glass", ]
+  glass = data_SG[data$Scale == "Glass", ]
   colnames(glass)[2:1698] = paste0("G", 402:2098)
   data = cbind(brown[, 1:1698], glass[, 2:1698])
 
